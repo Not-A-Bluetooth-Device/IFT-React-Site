5eb69ca4f06bbe06278a69a6d44707549a3aa74f
DMcough1024 <90269348+DMcough1024@users.noreply.github.com>
2023-09-22 16:17:20 -0500

    Create python-kicker.sh
    If placed in the proper folder, this will kick off the python script upon Raspberry Pi booting up.

---
+++ python-code/python-kicker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# update the cd as appropriate
+cd /home/pi/temperature_logger
+pyton temperature_logger.py
+
+# this shell script should be added to the /etc/rc.local file before the exit 0 line
